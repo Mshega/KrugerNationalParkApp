3b6e47fa39bbb8f86e7aac32469c11398c6e9d5b
Viwe Mrayise <122831469+Mshega@users.noreply.github.com>
2024-07-12 11:29:48 +0200

    Update DisplayUpdatedClientWildCards.cpp

--- DisplayUpdatedClientWildCards.cpp
+++ DisplayUpdatedClientWildCards.cpp
@@ -1,18 +1,9 @@
-#include <vcl.h>
-#pragma hdrstop
-
-#include "DisplayUpdatedClientWildCards.h"
-//---------------------------------------------------------------------------
-#pragma package(smart_init)
-#pragma resource "*.dfm"
-TForm1 *Form1;
-
 // Function to display updated client wildcards
-void __fastcall TForm1::DisplayClientWildcardsClick(TObject *Sender)
+void __fastcall TfrmKruger::btnShowLrecordClick(TObject *Sender)
 {
-    ADOUseQuery1->Close();
-    ADOUseQuery1->SQL->Clear();
-    ADOUseQuery1->SQL->Add("SELECT * FROM ClientWildCards");
-    ADOUseQuery1->Open();
-    ADOUseQuery1->Last(); // Move to the last record to make it visible
+    // make sure that the last (added) record is visible 
+    
+    DMod->ADOdsClientWildCards->Open(); 
+    DMod->ADOdsClientWildCards->Last(); 
+    dbgrdClientData->DataSource = DMod->DSClientWildCards;
 }
