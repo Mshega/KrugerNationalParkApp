56b6eeeefad73875cf7ae52b495005965248d771
Viwe Mrayise <122831469+Mshega@users.noreply.github.com>
2024-07-12 11:25:14 +0200

    GenerateNewWildcardNumber.cpp

--- GenerateNewWildcardNumber.cpp
+++ GenerateNewWildcardNumber.cpp
@@ -1,18 +1,39 @@
-#include <vcl.h>
-#pragma hdrstop
-#include "GenerateNewWildcardNumber.h"
-//---------------------------------------------------------------------------
-#pragma package(smart_init)
-#pragma resource "*.dfm"
-TForm1 *Form1;
-
-void __fastcall TForm1::GenerateNewWildcardNumberClick(TObject *Sender)
+void __fastcall TfrmKruger::btnWCnewNumClick(TObject *Sender)
 {
-    ADOdsTemp->Close();
-    ADOdsTemp->CommandText = "SELECT MAX(WildCardNr) AS LastWildCardNr FROM ClientWildCards";
-    ADOdsTemp->Open();
-    String lastWildCardNr = ADOdsTemp->FieldByName("LastWildCardNr")->AsString;
-    String newWildCardNr = "W" + IntToStr(StrToInt(lastWildCardNr.SubString(2, lastWildCardNr.Length()-1)) + 1);
-    lblLastWildCardNr->Caption = lastWildCardNr;
-    edtNewWildCardNr->Text = newWildCardNr;
+    pnlBottomData->Visible = true;                  
+	lblWCnum->Caption = "";                         
+	AnsiString zeroes = "000000"; 
+    
+    WCnum = "";
+    AnsiString WCtemp = "";
+    int WCnumNext = 0;
+
+    if (cmbWildcType->ItemIndex > -1)  // validate - a selection has been made
+    {
+        AnsiString TempSQL = "Select Max(WildCardNr) as LastNum from ClientWildCards ";
+        
+        DMod->ADOUseQuery2->Close();
+        DMod->ADOUseQuery2->SQL->Clear();
+        DMod->ADOUseQuery2->SQL->Add(TempSQL); 
+        DMod->ADOUseQuery2->Open();
+        
+        WCnum = DMod->ADOUseQuery2->Fields->FieldByName("LastNum")->AsAnsiString; 
+        lblWCnum->Caption = WCnum;
+        
+        // Generate the "next" record’s PK - WC number
+        // It must consist out of a leading "W" with 7 digits!
+        
+        WCtemp = WCnum.Delete(1,1); // delete leading "W"
+        int i = 1;
+   
+        while (WCtemp[i++] == '0') 
+            WCtemp.Delete(1,1);
+        
+        WCnumNext = StrToInt(WCtemp) + 1; 
+        WCnum = IntToStr(WCnumNext);
+        
+        WCnum = "W" + zeroes.SubString(1, 7- WCnum.Length()) + WCnum; 
+        edtWCnumNew->Text = WCnum;
+        btnInsert->Show(); 
+    }
 }
