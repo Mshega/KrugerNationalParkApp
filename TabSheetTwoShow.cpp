6fe523d0bbc6b3fc9dcec9a8ec816c4a39f1caf8
Viwe Mrayise <122831469+Mshega@users.noreply.github.com>
2024-07-12 11:17:13 +0200

    TabSheetTwoShow.cpp

--- TabSheetTwoShow.cpp
+++ TabSheetTwoShow.cpp
@@ -1,17 +1,10 @@
-#include <vcl.h>
-#pragma hdrstop
-#include "TabSheetTwoShow.h"
-//---------------------------------------------------------------------------
-#pragma package(smart_init)
-#pragma resource "*.dfm"
-TForm1 *Form1;
-
-void __fastcall TForm1::TabSheetTwoShow(TObject *Sender)
+void __fastcall TfrmKruger::tbsTwoShow(TObject *Sender)
 {
     cmbWildcType->Items->Clear();
     ADOdsWildCard->Close();
     ADOdsWildCard->Open();
-    while (!ADOdsWildCard->Eof) {
+    while (!ADOdsWildCard->Eof) 
+    {
         cmbWildcType->Items->Add(ADOdsWildCard->FieldByName("Code")->AsString + " - " + ADOdsWildCard->FieldByName("FullName")->AsString);
         ADOdsWildCard->Next();
     }
