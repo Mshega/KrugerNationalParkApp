98863bde8f9c35d19e431a24c1ea3911a181f0f8
Viwe Mrayise <122831469+Mshega@users.noreply.github.com>
2024-07-14 12:00:41 +0200

    InsertNewWildcard.cpp

--- InsertNewWildcard.cpp
+++ InsertNewWildcard.cpp
@@ -1,27 +1,42 @@
 // Function to insert a new WildCard record
 void __fastcall TfrmKruger::btnInsertClick(TObject *Sender)
 {
-    // insert with SQL add current date - use an appropriate SQL-related component
+    // Check if a WildCard type is selected in the combo box
     if (cmbWCtype->ItemIndex > -1) // validate selection
     {
+        // Extract WildCard type and amount paid from combo box selection
         AnsiString comboLn = cmbWCtype->Text;
-        AnsiString WCtype = comboLn.SubString(1, comboLn.Pos("-")-2); 
-        AnsiString paid = comboLn.SubString(comboLn.Pos("-")+3, 30);  //exclude'R'
+        AnsiString WCtype = comboLn.SubString(1, comboLn.Pos("-") - 2); 
+        AnsiString paid = comboLn.SubString(comboLn.Pos("-") + 3, 30);  // exclude 'R'
         
+        // Prepare SQL query to insert into ClientWildCards table
         AnsiString sql = "INSERT INTO ClientWildCards(WildCardNr, LastReviewalDate, WildCardType, ";
-            sql += " AmountPaid, MaxPax) VALUES (:WC, :Ldate, :WCT, :Paid, :MaxP)"; 
+        sql += "AmountPaid, MaxPax) VALUES (:WC, :Ldate, :WCT, :Paid, :MaxP)";
         
+        // Set the command text of ADOCommand to the SQL query
         DMod->ADOCommand->CommandText = sql;
-        DMod->ADOCommand->Parameters->ParamByName("WC")->Value = edtWCnumNew->Text;
-        DMod->ADOCommand->Parameters->ParamByName("Ldate")->Value = Date(); 
-        DMod->ADOCommand->Parameters->ParamByName("WCT")->Value = WCtype; 
-        DMod->ADOCommand->Parameters->ParamByName("Paid")->Value = paid; 
-        DMod->ADOCommand->Parameters->ParamByName("MaxP")->Value = IntToStr(spedtMaxP->Value); 
         
+        // Bind parameters to the query
+        DMod->ADOCommand->Parameters->ParamByName("WC")->Value = edtWCnumNew->Text;
+        DMod->ADOCommand->Parameters->ParamByName("Ldate")->Value = Date(); // Current date
+        DMod->ADOCommand->Parameters->ParamByName("WCT")->Value = WCtype;   // WildCard type
+        DMod->ADOCommand->Parameters->ParamByName("Paid")->Value = paid;    // Amount paid
+        DMod->ADOCommand->Parameters->ParamByName("MaxP")->Value = IntToStr(spedtMaxP->Value); // MaxPax
+        
+        // Execute the SQL command and check if it was successful
         if (DMod->ADOCommand->Execute() > 0) 
-            ShowMessage("New Client Wildcard recorded!"); 
+        {
+            // Show success message if the insertion was successful
+            ShowMessage("New Client Wildcard recorded!");
+        }
     }
     else
-        MessageDlg("First select a 'Wildcard Type'!", mtWarning, TMsgDlgButtons()<<mbOK, 0); 
-        btnShowLrecord->Show(); 
+    {
+        // Show a warning message if no WildCard type is selected
+        MessageDlg("First select a 'Wildcard Type'!", mtWarning, TMsgDlgButtons() << mbOK, 0);
+    }
+    
+    // Always show the 'Show' button after attempting to insert
+    btnShowLrecord->Show();
 }
+
