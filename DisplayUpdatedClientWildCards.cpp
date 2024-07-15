1394a8b0ea15baf70e0d2fa050b526f92aab3c69
Viwe Mrayise <122831469+Mshega@users.noreply.github.com>
2024-07-15 18:28:56 +0200

    DisplayUpdatedClientWildCards.cpp

--- DisplayUpdatedClientWildCards.cpp
+++ DisplayUpdatedClientWildCards.cpp
@@ -3,11 +3,7 @@ void __fastcall TfrmKruger::btnShowLrecordClick(TObject *Sender)
 {
     // Open the dataset containing ClientWildCards data
     DMod->ADOdsClientWildCards->Open();
-
-    // Move to the last record in the dataset to ensure the latest (added) record is visible
-    DMod->ADOdsClientWildCards->Last();
-
-    // Set the DataSource of dbgrdClientData to the dataset containing ClientWildCards data
-    dbgrdClientData->DataSource = DMod->DSClientWildCards;
+    DMod->ADOdsClientWildCards->Last();  // Move to the last record in the dataset to ensure the latest (added) record is visible
+    dbgrdClientData->DataSource = DMod->DSClientWildCards;  // Set the DataSource of dbgrdClientData to the dataset containing ClientWildCards data
 }
 
