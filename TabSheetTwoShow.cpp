517f1fa84faf7045816a978d283722bf4045d510
Viwe Mrayise <122831469+Mshega@users.noreply.github.com>
2024-07-26 12:17:12 +0200

    TabSheetTwoShow.cpp

--- TabSheetTwoShow.cpp
+++ TabSheetTwoShow.cpp
@@ -9,7 +9,6 @@ void __fastcall TfrmKruger::tbsTwoShow(TObject *Sender)
     while (!ADOdsWildCard->Eof) // Populate cmbWildcType with items from ADO dataset
     {
         cmbWildcType->Items->Add(ADOdsWildCard->FieldByName("Code")->AsString + " - " + ADOdsWildCard->FieldByName("FullName")->AsString); // Add each item as a combination of "Code - FullName" to cmbWildcType
-        
         ADOdsWildCard->Next(); // Move to the next record in the ADO dataset
     }
     cmbWildcType->Text = "Choose a Wildcard-type"; // Set the default text in cmbWildcType ComboBox
